60f13368ee81db2ad3e5ec579dcc5f041e8b7ef3
ankit607 <86359696+ankit607@users.noreply.github.com>
2022-04-10 07:35:51 -1200

    deleted items

--- test/test_terraform/non_runnable_test/test.tf
+++
@@ -1,9 +0,0 @@
-resource "null_resource" "test" {
-  count   = 1
-}
-
-
-output "test" {
-  # the variable below should be missing so it will break for testing non runnable terraform files behvior in the system
-  value = var.test
-}
